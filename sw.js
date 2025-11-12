fe6100b7fba94eb7baf76304e54979375fef24e1
merzouguisohaib0-del <merzougui.sohaib0@gmail.com>
2025-11-12 23:39:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-self.options = {
+elf.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10180626
+    "zoneId": 10180695
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
